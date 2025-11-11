7761908a43b6f7a56403e00f4ea052a1083ad5e5
Puran Lai <1696095803@qq.com>
2025-11-11 22:38:41 +0800

    Style: Refine about page layout and typography

--- src/components/About.vue
+++ src/components/About.vue
@@ -1,28 +1,30 @@
 <template>
   <div class="about-page">
     <h1>About</h1>
-    姓名：任思成
-    任务分工：网页设计
-    兴趣爱好：书法、国画、篮球等
-    游戏：主玩米游（原神、崩铁、绝区零）
-    现在计算机学院7班（不是人机）
+    <p>
+      姓名：任思成<br>
+      任务分工：网页设计<br>
+      兴趣爱好：书法、国画、篮球等<br>
+      游戏：主玩米游（原神、崩铁、绝区零）<br>
+      现在计算机学院7班（不是人机）<br>
+      <br>
 
-    
-    参与者：常逸飞
-    参与分工：图片收集及文案撰写
-    身份：计算机学院的大一学生一名，以及编程小白一枚。
-    感谢伟大的 赖朴然 ！！！
-    感谢伟大的 赖朴然 ！！！
-    感谢伟大的 赖朴然 ！！！
-    感谢伟大的 赖朴然 ！！！
-    感谢伟大的 赖朴然 ！！！
-    感谢伟大的 赖朴然 ！！！
-    感谢伟大的 赖朴然 ！！！
-    感谢伟大的 赖朴然 ！！！
-    感谢伟大的 赖朴然 ！！！
-    感谢伟大的 赖朴然 ！！！
-    感谢伟大的 赖朴然 ！！！
-    感谢伟大的 赖朴然 ！！！
+      参与者：常逸飞<br>
+      参与分工：图片收集及文案撰写<br>
+      身份：计算机学院的大一学生一名，以及编程小白一枚。<br>
+      感谢伟大的 赖朴然 ！！！<br>
+      感谢伟大的 赖朴然 ！！！<br>
+      感谢伟大的 赖朴然 ！！！<br>
+      感谢伟大的 赖朴然 ！！！<br>
+      感谢伟大的 赖朴然 ！！！<br>
+      感谢伟大的 赖朴然 ！！！<br>
+      感谢伟大的 赖朴然 ！！！<br>
+      感谢伟大的 赖朴然 ！！！<br>
+      感谢伟大的 赖朴然 ！！！<br>
+      感谢伟大的 赖朴然 ！！！<br>
+      感谢伟大的 赖朴然 ！！！<br>
+    </p>
+
   </div>
 </template>
 
@@ -30,14 +32,23 @@
 .about-page {
   background-color: var(--bg-color);
   min-height: 100vh;
-  padding: 60px;
+  padding-top: 60px;
+  padding-left: 6vw;
+  padding-bottom: 60px;
 }
 
 .about-page h1 {
   font-family: 'rtsxt', '宋体', sans-serif;
-  font-size: 2.5rem;
-  font-weight: 600;
+  font-size: 3.5rem;
+  font-weight: 500;
   color: var(--h1-color);
-  margin-bottom: 1rem;
+  margin-top: 2vw;
+  margin-bottom: 2rem;
+}
+
+.about-page p {
+  font-family: '宋体';
+  margin-left: 25px;
+  font-size: 24px;
 }
 </style>
